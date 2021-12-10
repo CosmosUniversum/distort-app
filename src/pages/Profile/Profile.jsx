f11371aa75e36ecd69fb9ef47be1e4aa47a49a98
Josh Castillo <John.Joshua.Castillo@gmail.com>
2021-12-09 20:57:42 -0500

    commit to edit api call styling

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom';
-import './Profile.css'
+import  './Profile.css'
 
 
 const Profile = ({loggedInUser}) => {
 const location = useLocation()
-const user = location.state
+const user = location.state 
 
     return (
         <>
         <main >
             <body>
+            <h1>{user.name}'s Profile Page</h1>
+            <div id="profilecontent">
             <h2>
             Name: {user.name}
             </h2>
@@ -24,15 +26,24 @@ const user = location.state
             <h2>
             Followers: {user.followers}
             </h2>
-            <h2>{loggedInUser.profile === user._id ? 'My ' : 'Their '} {user.lists?.length} </h2>
-            {loggedInUser.profile === user._id ? 
+            <h2>
+            Music List: {user.musicList}
+            </h2>
+            <h2>
+            Movie List: {user.movieList}
+            </h2>
+            <h2>
+            Podcast List: {user.podcastList}
+            </h2>
+            {loggedInUser.profile === user.profile ? 
             <form
             > 
-            {<Link to="/create-list-form" ><button>Create List</button></Link>}
+            {<Link to="/create-list" ><button id="listbtn">Create List</button></Link>}
             </form>
             :
             ''
             }
+            </div>
             </body> 
         </main>
     </>
